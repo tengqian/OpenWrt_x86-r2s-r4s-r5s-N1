89c2788d8fb73cc3692002ab813242a2cf8f34e2
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-12-25 15:33:49 +0800

    update

--- devices/ipq807x_ipq60xx/diy.sh
+++ devices/ipq807x_ipq60xx/diy.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-kernel_v="$(cat include/kernel-version.mk | grep LINUX_KERNEL_HASH-4.4.* | cut -f 2 -d - | cut -f 1 -d ' ')"
+kernel_v="$(cat include/kernel-version.mk | grep LINUX_KERNEL_HASH-5.4.164* | cut -f 2 -d - | cut -f 1 -d ' ')"
 echo "KERNEL=${kernel_v}" >> $GITHUB_ENV || true
 sed -i "s?targets/%S/.*'?targets/%S/$kernel_v'?" include/feeds.mk
 
